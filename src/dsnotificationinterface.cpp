1ec46383137c6923344100ac9337c9333c356869
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-07-10 17:42:09 +0300

    fixed conflict

--- src/dsnotificationinterface.cpp
+++ src/dsnotificationinterface.cpp
@@ -4,12 +4,12 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "dsnotificationinterface.h"
-#include "privatesend-client.h"
 #include "instantx.h"
 #include "governance.h"
 #include "masternodeman.h"
 #include "masternode-payments.h"
 #include "masternode-sync.h"
+#include "privatesend-client.h"
 
 CDSNotificationInterface::CDSNotificationInterface()
 {
@@ -32,4 +32,5 @@ void CDSNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindex)
 void CDSNotificationInterface::SyncTransaction(const CTransaction &tx, const CBlock *pblock)
 {
     instantsend.SyncTransaction(tx, pblock);
+    CPrivateSend::SyncTransaction(tx, pblock);
 }

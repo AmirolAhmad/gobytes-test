e276758372389777be29bb068dbe47660f5d8826
gladcow <sergey@dash.org>
2017-07-17 07:38:32 -0700

    change invalid version string constant (#1532)

--- src/util.cpp
+++ src/util.cpp
@@ -1000,7 +1000,7 @@ std::string SafeIntVersionToString(uint32_t nVersion)
     }
     catch(const std::bad_cast&)
     {
-        return "Invalid version";
+        return "invalid_version";
     }
 }
 

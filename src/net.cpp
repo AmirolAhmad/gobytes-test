ee883d4dc7fe3e8bc91ba722fb418dd89543a2d6
Oleg Girko <ol+github@infoserver.lv>
2017-09-02 21:07:11 +0100

    Backport Bitcoin PR#7868: net: Split DNS resolving functionality out of net structures (#1601)
    * net: require lookup functions to specify all arguments
    
    To make it clear where DNS resolves are happening
    
    * net: manually resolve dns seed sources
    
    Note: Some seeds aren't actually returning an IP for their name entries, so
    they're being added to addrman with a source of [::].
    
    This commit shouldn't change that behavior, for better or worse.
    
    * net: resolve outside of storage structures
    
    Rather than allowing CNetAddr/CService/CSubNet to launch DNS queries, require
    that addresses are already resolved.
    
    This greatly simplifies async resolve logic, and makes it harder to
    accidentally leak DNS queries.
    
    * net: disable resolving from storage structures
    
    CNetAddr/CService/CSubnet can no longer resolve DNS.

--- src/net.cpp
+++ src/net.cpp
@@ -1524,7 +1524,7 @@ void CConnman::ThreadDNSAddressSeed()
         } else {
             std::vector<CNetAddr> vIPs;
             std::vector<CAddress> vAdd;
-            if (LookupHost(seed.host.c_str(), vIPs))
+            if (LookupHost(seed.host.c_str(), vIPs, 0, true))
             {
                 BOOST_FOREACH(const CNetAddr& ip, vIPs)
                 {
@@ -1535,7 +1535,15 @@ void CConnman::ThreadDNSAddressSeed()
                     found++;
                 }
             }
-            addrman.Add(vAdd, CNetAddr(seed.name, true));
+            // TODO: The seed name resolve may fail, yielding an IP of [::], which results in
+            // addrman assigning the same source to results from different seeds.
+            // This should switch to a hard-coded stable dummy IP for each seed name, so that the
+            // resolve is not required at all.
+            if (!vIPs.empty()) {
+                CService seedSource;
+                Lookup(seed.name.c_str(), seedSource, 0, true);
+                addrman.Add(vAdd, seedSource);
+            }
         }
     }
 
@@ -2040,7 +2048,7 @@ void Discover(boost::thread_group& threadGroup)
     if (gethostname(pszHostName, sizeof(pszHostName)) != SOCKET_ERROR)
     {
         std::vector<CNetAddr> vaddr;
-        if (LookupHost(pszHostName, vaddr))
+        if (LookupHost(pszHostName, vaddr, 0, true))
         {
             BOOST_FOREACH (const CNetAddr &addr, vaddr)
             {

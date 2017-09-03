0c7ba3d7f5735d6008deb73f096b8ee28f4a5a11
Oleg Girko <ol+github@infoserver.lv>
2017-09-03 14:29:10 +0100

    Backport Bitcoin PR#8128: Net: Turn net structures into dumb storage classes (#1604)
    * net: Split resolving out of CNetAddr
    
    * net: Split resolving out of CService
    
    * net: Split resolving out of CSubNet
    
    * net: move CNetAddr/CService/CSubNet out of netbase
    
    * net: narrow include scope after moving to netaddress
    
    Net functionality is no longer needed for CAddress/CAddrman/etc. now that
    CNetAddr/CService/CSubNet are dumb storage classes.
    
    * net: Add direct tests for new CSubNet constructors
    
    * net: Have LookupNumeric return a CService directly
    
    Also fix up a few small issues:
    - Lookup with "badip:port" now sets the port to 0
    - Don't allow assert to have side-effects
    
    * net: fixup nits

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -17,6 +17,7 @@
 #include "init.h"
 #include "validation.h" // For DEFAULT_SCRIPTCHECK_THREADS
 #include "net.h"
+#include "netbase.h"
 #include "txdb.h" // for -dbcache defaults
 
 #ifdef ENABLE_WALLET
